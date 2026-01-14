6408bb4cedac8ad9dcf9c1c3be676bee980e1511
goerch <jhr.walter@t-online.de>
2026-01-14 15:33:54 +0100

    Bugfix

--- basic.py
+++ basic.py
@@ -1,16 +1,18 @@
 import sys
-from llmutils import chat, chat_stream
+from llmutils import assemble_messages, chat, chat_stream
 
 
 def run_chat(user_prompt):
     print(user_prompt)
-    status, thinking, content = chat(None, user_prompt)
+    messages = assemble_messages(None, user_prompt)
+    status, thinking, content = chat(messages)
     print(status, thinking, content)
 
 
 def run_chat_stream(user_prompt):
     print(user_prompt)
-    for status, thinking, content in chat_stream(None, user_prompt):
+    messages = assemble_messages(None, user_prompt)
+    for status, thinking, content in chat_stream(messages):
         assert status == 200
         if thinking:
             print(thinking, end="", flush=True)
